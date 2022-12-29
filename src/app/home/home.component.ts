059a1bb1f4f75f31dd69c2e0d5094394c27a8fef
MoulinaPradhan <pmoulina76@gmail.com>
2022-12-29 11:13:49 +0530

    add three sub category functionalities

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,10 +10,11 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent implements OnInit {
   public eventList: any;
   event_category="ALL_EVENTS";
+  event_sub_category="Upcoming";
  
-   url='https://api.codingninjas.com/api/v3/events?event_category='+this.event_category+'&'+'event_sub_category=Upcoming&tag_list=&offset=0';
+   url='https://api.codingninjas.com/api/v3/events?event_category='+this.event_category+'&'+'event_sub_category='+this.event_sub_category+'&tag_list=&offset=0'
  
-   // public event_sub_category="Upcoming";
+ 
    // public tag_list="Coding%20Concepts";
    // public offset=1;
   
@@ -57,10 +58,28 @@ export class HomeComponent implements OnInit {
    solve_work(){
      console.log("clicked");
      this.event_category="WORKSHOP"
-     this.url='https://api.codingninjas.com/api/v3/events?event_category='+this.event_category+'&'+'event_sub_category=Upcoming&tag_list=&offset=0';
+     this.url=this.url;
      console.log(this.url);
      this.getevents();
    }
+
+   getArchive(){
+    this.event_sub_category = "Archived";
+    this.url = 'https://api.codingninjas.com/api/v3/events?event_category='+this.event_category+'&'+'event_sub_category='+this.event_sub_category+'&tag_list=&offset=0';
+    this.getevents(); 
+  }
+
+  getUpcoming(){
+    this.event_sub_category = "Upcoming";
+    this.url = 'https://api.codingninjas.com/api/v3/events?event_category='+this.event_category+'&'+'event_sub_category='+this.event_sub_category+'&tag_list=&offset=0';
+    this.getevents(); 
+  }
+
+  getAllTimeFavorites(){
+    this.event_sub_category = "All Time Favorites";
+    this.url = 'https://api.codingninjas.com/api/v3/events?event_category='+this.event_category+'&'+'event_sub_category='+this.event_sub_category+'&tag_list=&offset=0';
+    this.getevents(); 
+  }
   
    public getevents(){
      this.http.get(this.url).subscribe((data) =>{
