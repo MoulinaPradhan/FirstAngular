bf36ef97a90d143f1151e96044b25ebae6a37115
MoulinaPradhan <pmoulina76@gmail.com>
2023-05-30 10:15:49 +0530

    What is Angular Material Layout Directive?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 
-import { EventComponent } from './event/event.component';
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Coding Ninjs!';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,36 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { TagsComponent } from './tags/tags.component';
-import { HttpClientModule } from '@angular/common/http';
-import { EventComponent } from './event/event.component';
-import { RouterModule,Routes } from '@angular/router';
-import { NgxPaginationModule } from 'ngx-pagination'; 
 
-const routes: Routes=[
-  {
-    path : 'events/:event_category',
-     component: EventComponent}
-]
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HeaderComponent,
-    FooterComponent,
-    TagsComponent,
-    EventComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgxPaginationModule,
-    RouterModule.forRoot(routes)
+
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
